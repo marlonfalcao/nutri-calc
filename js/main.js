0306ea88336e166fcc34e269114ff54df9def84f
Marlon Santos <marlon.falcao@icloud.com>
2022-04-05 11:26:21 -0300

    Aplicando um pouco mais de conhecimento sobre array, toFixed, classList.add, for

--- js/main.js
+++ js/main.js
@@ -1,25 +1,34 @@
 let titulo = document.querySelector("#titulo");
 titulo.textContent = "Aparecida Nutricionista"
 
-let paciente = document.querySelector("#primeiro-paciente");
-let peso = paciente.querySelector(".info-peso").textContent;
-let altura = paciente.querySelector(".info-altura").textContent;
+let pacientes = document.querySelectorAll(".paciente");
 
-let imc = paciente.querySelector(".info-imc")
-let pesoEhValido = true
-let alturaEhValida = true
+for (let i = 0; i < pacientes.length; i++) {
+    const paciente = pacientes[i];
 
-if (peso <= 0 || peso >= 1000) {
+    let peso = paciente.querySelector(".info-peso").textContent;
+    let altura = paciente.querySelector(".info-altura").textContent;
+
+    let imc = paciente.querySelector(".info-imc")
+    let pesoEhValido = true
+    let alturaEhValida = true
+
+    if (peso <= 0 || peso >= 1000) {
     pesoEhValido = false
     imc.textContent = "Peso inválido!"
-}
+    
+    paciente.classList.add("paciente-invalido")
+    }
 
-if (altura <= 0 || altura >= 3.00) {
+    if (altura <= 0 || altura >= 3.00) {
     alturaEhValida = false
     imc.textContent = "Altura inválida!"
-}
 
-if (alturaEhValida && pesoEhValido) {
+    paciente.classList.add("paciente-invalido")
+    }
+
+    if (alturaEhValida && pesoEhValido) {
     let resultadoImc = peso / (altura * altura);
-    imc.textContent = resultadoImc
+    imc.textContent = resultadoImc.toFixed(1)
+    }
 }
