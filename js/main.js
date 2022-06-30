42de35246e419569f250db53ebb450993a13866a
Marlon Dos Santos <marlondossantos@voegol.com.br>
2022-06-29 22:54:53 -0300

    variáveis, validações com condições e operadores

--- js/main.js
+++ js/main.js
@@ -1,2 +1,29 @@
 let titulo = document.querySelector("#titulo");
-console.log(titulo.textContent)
+titulo.textContent = "Nutricionista Aparecida"
+
+//fórmula IMC IMC = PESO / (ALTURA X ALTURA)
+
+let paciente = document.querySelector("#primeiro-paciente")
+let peso = paciente.querySelector(".info-peso").textContent
+let altura = paciente.querySelector(".info-altura").textContent
+var tdImc = paciente.querySelector(".info-imc")
+
+let pesoValido = true;
+let alturaValida = true;
+
+if (peso <= 0 || peso >= 1000){
+    console.log("Peso inválido!")
+    pesoValido = false
+    tdImc.textContent = "Peso inválido"
+}
+
+if (altura <= 0 || altura >= 3){
+    console.log("Altura inválida!")
+    alturaValida = false
+    tdImc.textContent = "Altura inválida"
+}
+
+if (pesoValido && alturaValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc
+}
