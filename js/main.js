5576ccb8c306ec9c70b107b621f8c8f06cc40121
Marlon Dos Santos <marlondossantos@voegol.com.br>
2022-07-01 15:29:06 -0300

    index

--- js/main.js
+++ js/main.js
@@ -1,29 +1,33 @@
 let titulo = document.querySelector("#titulo");
 titulo.textContent = "Nutricionista Aparecida"
 
-//fórmula IMC IMC = PESO / (ALTURA X ALTURA)
+let pacientes = document.querySelectorAll(".paciente")
 
-let paciente = document.querySelector("#primeiro-paciente")
-let peso = paciente.querySelector(".info-peso").textContent
-let altura = paciente.querySelector(".info-altura").textContent
-var tdImc = paciente.querySelector(".info-imc")
+for(let i = 0; i < pacientes.length; i++){
+    let paciente = pacientes[i]
 
-let pesoValido = true;
-let alturaValida = true;
+    let peso = paciente.querySelector(".info-peso").textContent
+    let altura = paciente.querySelector(".info-altura").textContent
+    var tdImc = paciente.querySelector(".info-imc")
 
-if (peso <= 0 || peso >= 1000){
-    console.log("Peso inválido!")
-    pesoValido = false
-    tdImc.textContent = "Peso inválido"
+    let pesoValido = true;
+    let alturaValida = true;
+
+    if (peso <= 0 || peso >= 1000){
+        console.log("Peso inválido!")
+        pesoValido = false
+        tdImc.textContent = "Peso inválido"
+    }
+
+    if (altura <= 0 || altura >= 3){
+        console.log("Altura inválida!")
+        alturaValida = false
+        tdImc.textContent = "Altura inválida"
+    }
+
+    if (pesoValido && alturaValida){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(1)
+    }
 }
 
-if (altura <= 0 || altura >= 3){
-    console.log("Altura inválida!")
-    alturaValida = false
-    tdImc.textContent = "Altura inválida"
-}
-
-if (pesoValido && alturaValida){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc
-}
